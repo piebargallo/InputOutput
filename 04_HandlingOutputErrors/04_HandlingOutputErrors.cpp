11fe6dbaaef3975f4a2c9dce4906db4c2c3f47a4
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-07-21 11:13:34 +0200

    Add files via upload

---
+++ 04_HandlingOutputErrors/04_HandlingOutputErrors.cpp
@@ -0,0 +1,24 @@
+// C++ code to demonstrate
+// the working of good() function
+
+#include <sstream>
+#include <iostream>
+using std::stringstream;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    // Stream
+    stringstream ss;
+
+    // Using good() function
+    bool isGood = ss.good();
+
+    // print result
+    cout << "is stream good: "
+        << isGood << endl;
+
+    return 0;
+}
