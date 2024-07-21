9bfc7a6300de65dac84f40116211bc752e8743e8
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-07-21 15:54:08 +0200

    Add files via upload

---
+++ 07_InputBasics/07_InputBasics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+	string userInput;
+	cout << "Xafa userInput(string): ";
+	cin >> userInput;
+	cout << "User Input was: " << userInput << endl;
+	return 0;
+}
